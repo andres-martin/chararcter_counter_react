42d137abd36159606e30289ee9c5080641ca1a5e
andres-martin <andresmartin266@gmail.com>
2019-09-04 22:27:26 -0500

    characters counter done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.counter = this.counter.bind(this)
+
+    this.state = { value: 0 }
+
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.counter}></textarea>
+        <div className="counter" >{this.state.value}</div>
       </div>
     );
+  }
+  // this function handles an event then gets their value and then we can calculate its length
+  counter(e) {
+    this.setState({ value: e.target.value.length });
   }
 }
 
